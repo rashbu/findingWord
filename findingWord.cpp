1b71a79324a5fd6340d61011660e1245ca512570
Rafał Ściebura <rafal.sciebura7@gmail.com>
2022-07-01 03:58:09 +0200

    Add new files

---
+++ findingWord.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string writing = "Ala ma kota";
+    std::string search = "kot";
+
+    size_t position = writing.find(search);
+
+    if (position != std::string::npos) {
+        std::cout << "Found in position: " << position;
+    }
+    else {
+        std::cout << "Not found!";
+    }
+    
+    return 0;
+}
